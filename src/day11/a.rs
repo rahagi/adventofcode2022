0272fda494892ccedaa4e7a1caa08acc8f13be23
Rahagi <rahagi@protonmail.com>
2022-12-11 15:34:01 +0700

    feat: day11a

---
+++ src/day11/a.rs
@@ -0,0 +1,60 @@
+use std::{collections::BTreeMap, cell::RefCell, str::FromStr};
+use super::monkey::{Monkey, Operation, Id};
+use crate::utils;
+
+const ROUND_REQUIRED: usize = 20;
+
+pub fn a(input_file: &str) -> usize {
+    let input = utils::file::file_to_str(input_file);
+
+    let mut monkeys: BTreeMap<Id, RefCell<Monkey>> = BTreeMap::new();
+    input
+        .split("\n\n")
+        .enumerate()
+        .for_each(|(id, monkey)| {
+            let monkey = Monkey::from_str(monkey).unwrap();
+            monkeys.insert(id, RefCell::new(monkey));
+        });
+    let mut inspect_count = vec![0; monkeys.len()];
+
+    
+    let mut round = 1;
+    while round <= ROUND_REQUIRED {
+        monkeys.iter().for_each(|(id, monkey)| {
+            let mut monkey = monkey.borrow_mut();
+
+            while let Some(worry_level) = monkey.stack.pop() {
+                let mut item_value = match monkey.operation {
+                    Operation::Multiply(Some(arg)) => worry_level * arg,
+                    Operation::Multiply(None) => worry_level * worry_level,
+                    Operation::Add(Some(arg)) => worry_level + arg,
+                    Operation::Add(None) => worry_level + worry_level,
+                };
+                item_value /= 3;
+
+                let next_id = if item_value % monkey.test == 0 {
+                    monkey.next_if_true
+                } else {
+                    monkey.next_if_false
+                };
+
+                monkeys.get(&next_id).unwrap().borrow_mut().stack.push(item_value);
+                inspect_count[*id] += 1;
+            }
+        });
+        round += 1;
+    }
+
+    inspect_count.sort();
+    inspect_count[inspect_count.len() - 2..].iter().product()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::a;
+    #[test]
+    fn test_11_a() {
+        assert_eq!(a("src/day11/input_example.txt"), 10605);
+        println!("Answer: {}", a("src/day11/input.txt"));
+    }
+}
---
+++ src/day11/monkey.rs
@@ -0,0 +1,92 @@
+use std::str::FromStr;
+
+#[derive(Debug)]
+pub enum Operation {
+    Multiply(Option<usize>),
+    Add(Option<usize>),
+}
+
+pub type Id = usize;
+
+#[derive(Debug)]
+pub struct Monkey {
+    pub operation: Operation,
+    pub stack: Vec<usize>,
+    pub test: usize,
+    pub next_if_true: Id,
+    pub next_if_false: Id,
+}
+
+impl Monkey {
+    pub fn new() -> Monkey {
+        Monkey {
+            operation: Operation::Multiply(None),
+            stack: Vec::new(),
+            test: 0,
+            next_if_true: 0,
+            next_if_false: 0,
+        }
+    }
+
+    fn load_stack(&mut self, stack: &str) {
+        let parts = stack.split(": ").nth(1).unwrap();
+        self.stack = parts
+            .rsplit(", ")
+            .map(|x| x.parse::<usize>().unwrap())
+            .collect();
+    }
+
+    fn load_operation(&mut self, operation: &str) {
+        let parts = operation.split(": ").nth(1).unwrap();
+        let arg = parts.split(|c| c == '*' || c == '+').nth(1).unwrap().trim();
+
+        if let Ok(arg) = arg.parse::<usize>() {
+            if operation.contains('*') {
+                self.operation = Operation::Multiply(Some(arg));
+            } else {
+                self.operation = Operation::Add(Some(arg));
+            }
+        } else if operation.contains('*') {
+            self.operation = Operation::Multiply(None);
+        } else {
+            self.operation = Operation::Add(None);
+        }
+    }
+
+    fn load_test(&mut self, test: &str) {
+        let parts = test.split(' ').last().unwrap();
+        self.test = parts.parse().unwrap();
+    }
+
+    fn load_next_if_true(&mut self, next_if_true: &str) {
+        let parts = next_if_true.split(' ').last().unwrap();
+        self.next_if_true = parts.parse().unwrap();
+    }
+
+    fn load_next_if_false(&mut self, next_if_false: &str) {
+        let parts = next_if_false.split(' ').last().unwrap();
+        self.next_if_false = parts.parse().unwrap();
+    }
+}
+
+impl FromStr for Monkey {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut monkey = Monkey::new();
+        let mut lines = s.lines().skip(1);
+        let stack = lines.next().unwrap();
+        let operation = lines.next().unwrap();
+        let test = lines.next().unwrap();
+        let next_if_true = lines.next().unwrap();
+        let next_if_false = lines.next().unwrap();
+
+        monkey.load_stack(stack);
+        monkey.load_operation(operation);
+        monkey.load_test(test);
+        monkey.load_next_if_true(next_if_true);
+        monkey.load_next_if_false(next_if_false);
+
+        Ok(monkey)
+    }
+}
